38f43540869335fbc0a83b60de90298e5a91e2fe
Richmond Gyebi <gyebirichmond21@gmail.com>
2025-12-08 15:47:13 +0000

    Add slide-out animation

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const hamburgerButton = document.querySelector(".hamburger");
 const navContainer = document.querySelector(".nav-container");
+const buttonSpans = document.querySelectorAll("button span");
 
 
 hamburgerButton.addEventListener("click", () => {
   navContainer.classList.toggle("active");
+
+  buttonSpans.forEach(span => span.classList.toggle("active"));
 });
