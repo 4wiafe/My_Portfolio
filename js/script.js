f920508882e4c48ecd2f9307b7925185c9d8de44
Richmond Wiafe Gyebi <gyebirichmond21@gmail.com>
2025-12-22 19:02:57 +0000

    Add functionality to menu button

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
 const hamburgerBtn = document.querySelector(".hamburger");
 const buttonSpans = document.querySelectorAll("button span");
+const navContainer = document.querySelector(".nav-container");
 
 hamburgerBtn.addEventListener("click", () => {
   buttonSpans.forEach(span => {
     span.classList.toggle("active");
   });
+
+  navContainer.classList.toggle("active");
 });
 
