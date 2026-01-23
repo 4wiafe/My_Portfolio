8e97a58254202f60df9759901702e5805780a8b0
Richmond Wiafe Gyebi <gyebirichmond21@gmail.com>
2026-01-23 15:01:34 +0000

    Style the header and navigation links

--- js/script.js
+++ js/script.js
@@ -9,6 +9,7 @@ function getSavedTheme() {
     const savedTheme = localStorage.getItem(THEME_KEY);
 
     if (savedTheme === "light" || savedTheme === "dark") {
+      updateThemeIcon(savedTheme);
       return savedTheme;
     }
 
@@ -16,7 +17,13 @@ function getSavedTheme() {
       "(prefers-color-scheme: dark)"
     ).matches;
 
-    return prefersDark ? "dark" : "light";
+    if (prefersDark) {
+      updateThemeIcon("dark");
+      return "dark";
+    } else {
+      updateThemeIcon("light");
+      return "light";
+    }
 
   } catch (error) {
     console.error("Theme detection failed", error);
@@ -32,5 +39,12 @@ themeToggle.addEventListener("click", () => {
   const newTheme = currentTheme === "light" ? "dark" : "light";
 
   rootElement.setAttribute("data-theme", newTheme);
+  updateThemeIcon(newTheme);
   localStorage.setItem(THEME_KEY, newTheme);
 });
+
+function updateThemeIcon(theme) {
+  return theme === "light" ? 
+    themeIcon.className = "fa-regular fa-moon" : 
+    themeIcon.className = "fa-regular fa-sun";
+}
